355e8ca66aa9e9d420f1f37a342e6ed7f2f0db58
Teeratorn Thiengtham <tew@Teeratorns-MacBook-Air.local>
2025-11-03 21:46:41 +0700

    fixbug(fe): Enable CORS to integrage with NestJS

--- backend-aivana/src/main.ts
+++ backend-aivana/src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  
+  // Enable CORS
+  app.enableCors({
+    origin: true,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+  
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
