69cb9ba339b8c9cb0ca940f11974dc09ab63c245
bisbat <baitong.sae-@mail.kmutt.ac.th>
2025-11-18 15:09:13 +0700

    feat(fe): update EditProductPage layout and import ProductForm component

--- frontend-aivana/app/stores/products/[productId]/edit/page.tsx
+++ frontend-aivana/app/stores/products/[productId]/edit/page.tsx
@@ -3,12 +3,19 @@ import { useState, useEffect, ChangeEvent, FormEvent } from 'react';
 import { useParams, useRouter } from 'next/navigation';
 import { ProductUpdatePayload } from '@/lib/types/product/UpdateProductPayload';
 import { ProductImages } from '@/lib/types/product/product_images';
+import { ProductForm } from '@/components/products/ProductForm';
 
 export default function EditProductPage() {
 
+  
+
   return (
     <div>
-      <h1>Edit page</h1>  
+      <div>
+        <button>Back</button>
+      </div>
+      <h1>Edit page</h1>
+
     </div>
   );
 }
