f40bcd150b2c163dfb5e25209b527ef9a10c8294
Tew2945 <tew2945@gmail.com>
2025-11-27 19:11:49 +0700

    refactor(be): remove user module and change to auth endpoint naming

--- backend-aivana/src/app.module.ts
+++ backend-aivana/src/app.module.ts
@@ -12,6 +12,7 @@ import { TagsModule } from './tags/tags.module';
 import { AuthModule } from './auth/auth.module';
 import { SellersModule } from './sellers/sellers.module';
 
+
 @Module({
   imports: [
     ConfigModule.forRoot({
