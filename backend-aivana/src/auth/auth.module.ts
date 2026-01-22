4bde24f5dcdc88e21e806fce0c2c1e5649ba91b5
bisbat <baitong.sae-@mail.kmutt.ac.th>
2026-01-22 20:50:21 +0700

    fix: update import paths for LocalStrategy and JwtStrategy in AuthModule

--- backend-aivana/src/auth/auth.module.ts
+++ backend-aivana/src/auth/auth.module.ts
@@ -4,8 +4,8 @@ import { JwtModule } from '@nestjs/jwt';
 import { JWT_SECRET } from './config/jwt-secret';
 import { PassportModule } from '@nestjs/passport';
 import { PassportAuthController } from './passport-auth.controller';
-import { LocalStrategy } from 'src/common/strategies/local.strategy';
-import { JwtStrategy } from 'src/common/strategies/jwt.strategy';
+import { LocalStrategy } from './strategies/local.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
 import { UserModule } from 'src/user/user.module';
 import { MinioModule } from 'src/minio/minio.module';
 
