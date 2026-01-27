3a777925173b7a1d009b8dbe42d4d71ceb0fa1a7
bisbat <baitong.sae-@mail.kmutt.ac.th>
2026-01-27 16:26:10 +0700

    refactor: remove checkout module, controller, service, and related DTOs

--- backend-aivana/src/checkout/dto/create-checkout.dto.ts
+++
@@ -1,3 +0,0 @@
-export class CreateCheckoutDto {
-    productIds: number[];
-}
--- backend-aivana/src/checkout/dto/res-checkout.dto.ts
+++
@@ -1,7 +0,0 @@
-export class ResCheckoutDto {
-    checkoutId: number;
-    userId: string;
-    productIds: number[];
-    totalAmount: number;
-    createdAt: Date;
-}
